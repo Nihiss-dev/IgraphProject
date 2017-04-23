697f83669f28d9985e02da8d215ed9d8c9602a9c
ALFEL5 <alexis.feldmar.1@ulaval.ca>
2017-04-23 08:49:14 +0200

    Hermite curve

--- Project/CurvesBar.cpp
+++ Project/CurvesBar.cpp
@@ -1,6 +1,7 @@
 #include "CurvesBar.h"
 
-CurvesBar::CurvesBar(Renderer *renderer) : m_renderer(renderer), m_visible(false), m_BezierEnabled(false), m_drawBezier(false)
+CurvesBar::CurvesBar(Renderer *renderer) : m_renderer(renderer), m_visible(false), m_BezierEnabled(false), m_drawBezier(false),
+	m_HermiteEnabled(false), m_drawHermite(false)
 {
 
 }
@@ -20,20 +21,46 @@ void	CurvesBar::Setup()
 	m_BezierCurvesPanel.setName("Create Bezier curve");
 	m_BezierCurvesPanel.setPosition(440,0);
 
+	m_HermiteCurvesPanel.setup();
+	m_HermiteCurvesPanel.setName("Create Hermite curve");
+	m_HermiteCurvesPanel.setPosition(440,0);
+
 	m_panel.add(m_BezierCubicButton.setup("Add Bezier Cubic Curve"));
+	m_panel.add(m_HermiteCubicButton.setup("Add Hermite Cubic Curve"));
 
 	m_BezierCurvesPanel.add(m_BezierPt1X.setup("Bezier Point1 X", 0, -500, 500));
 	m_BezierCurvesPanel.add(m_BezierPt1Y.setup("Bezier Point1 Y", 0, -500, 500));
+	m_BezierCurvesPanel.add(m_BezierPt1Z.setup("Bezier Point1 Z", 0, -500, 500));
 	m_BezierCurvesPanel.add(m_BezierPt2X.setup("Bezier Point2 X", 0, -500, 500));
 	m_BezierCurvesPanel.add(m_BezierPt2Y.setup("Bezier Point2 Y", 0, -500, 500));
+	m_BezierCurvesPanel.add(m_BezierPt2Z.setup("Bezier Point2 Z", 0, -500, 500));
 	m_BezierCurvesPanel.add(m_BezierPt3X.setup("Bezier Point3 X", 0, -500, 500));
-	m_BezierCurvesPanel.add(m_BezierPt3Y.setup("Bezier Point2 Y", 0, -500, 500));
+	m_BezierCurvesPanel.add(m_BezierPt3Y.setup("Bezier Point3 Y", 0, -500, 500));
+	m_BezierCurvesPanel.add(m_BezierPt3Z.setup("Bezier Point3 Z", 0, -500, 500));
 	m_BezierCurvesPanel.add(m_BezierPt4X.setup("Bezier Point4 X", 0, -500, 500));
-	m_BezierCurvesPanel.add(m_BezierPt4Y.setup("Bezier Point2 Y", 0, -500, 500));
+	m_BezierCurvesPanel.add(m_BezierPt4Y.setup("Bezier Point4 Y", 0, -500, 500));
+	m_BezierCurvesPanel.add(m_BezierPt4Z.setup("Bezier Point4 Z", 0, -500, 500));
 	m_BezierCurvesPanel.add(m_CreateBezier.setup("Draw Bezier Curve"));
 
+	m_HermiteCurvesPanel.add(m_HermitePt1X.setup("Hermite Point1 X", 0, -500, 500));
+	m_HermiteCurvesPanel.add(m_HermitePt1Y.setup("Hermite Point1 Y", 0, -500, 500));
+	m_HermiteCurvesPanel.add(m_HermitePt1Z.setup("Hermite Point1 Z", 0, -500, 500));
+	m_HermiteCurvesPanel.add(m_HermitePt2X.setup("Hermite Point2 X", 0, -500, 500));
+	m_HermiteCurvesPanel.add(m_HermitePt2Y.setup("Hermite Point2 Y", 0, -500, 500));
+	m_HermiteCurvesPanel.add(m_HermitePt2Z.setup("Hermite Point2 Z", 0, -500, 500));
+	m_HermiteCurvesPanel.add(m_HermiteVct1X.setup("Hermite Vector1 X", 0, -500, 500));
+	m_HermiteCurvesPanel.add(m_HermiteVct1Y.setup("Hermite Vector1 Y", 0, -500, 500));
+	m_HermiteCurvesPanel.add(m_HermiteVct1Z.setup("Hermite Vector1 Z", 0, -500, 500));
+	m_HermiteCurvesPanel.add(m_HermiteVct2X.setup("Hermite Vector2 X", 0, -500, 500));
+	m_HermiteCurvesPanel.add(m_HermiteVct2Y.setup("Hermite Vector2 Y", 0, -500, 500));
+	m_HermiteCurvesPanel.add(m_HermiteVct2Z.setup("Hermite Vector2 Z", 0, -500, 500));
+	m_HermiteCurvesPanel.add(m_CreateHermite.setup("Draw Hermite Curve"));
+
+
 	m_BezierCubicButton.addListener(this, &CurvesBar::AddBezierCubicCurve);
+	m_HermiteCubicButton.addListener(this, &CurvesBar::AddHermiteCubicCurve);
 	m_CreateBezier.addListener(this, &CurvesBar::DrawBezierCurve);
+	m_CreateHermite.addListener(this, &CurvesBar::DrawHermiteCubicCurve);
 }
 
 void	CurvesBar::Draw()
@@ -42,6 +69,8 @@ void	CurvesBar::Draw()
 		m_panel.draw();
 	if (m_BezierEnabled)
 		m_BezierCurvesPanel.draw();
+	if (m_HermiteEnabled)
+		m_HermiteCurvesPanel.draw();
 	if (m_drawBezier)
 	{
 		ofSetHexColor(0xFF0000);
@@ -52,6 +81,8 @@ void	CurvesBar::Draw()
 			(float)m_BezierPt4X, (float)m_BezierPt4Y);
 		ofSetHexColor(0xFFFFFF);
 	}
+	if (m_drawHermite)
+		DrawHermite();
 }
 
 void	CurvesBar::Show()
@@ -64,6 +95,7 @@ void	CurvesBar::Hide()
 	m_visible = false;
 	m_BezierEnabled = false;
 	m_drawBezier = false;
+	m_HermiteEnabled = false;
 }
 
 void	CurvesBar::AddBezierCubicCurve()
@@ -71,7 +103,34 @@ void	CurvesBar::AddBezierCubicCurve()
 	m_BezierEnabled = true;
 }
 
+void	CurvesBar::AddHermiteCubicCurve()
+{
+	m_HermiteEnabled = true;
+}
+
 void	CurvesBar::DrawBezierCurve()
 {
 	m_drawBezier = true;
+}
+
+void	CurvesBar::DrawHermiteCubicCurve()
+{
+	m_drawHermite = true;
+}
+
+void	CurvesBar::DrawHermite()
+{
+	ofPoint Pt1(m_HermitePt1X, m_HermitePt1Y, m_HermitePt1Z);
+	ofPoint Pt2(m_HermitePt2X, m_HermitePt2Y, m_HermitePt2Z);
+	ofVec3f V1(m_HermiteVct1X, m_HermiteVct1Y, m_HermiteVct1Z);
+	ofVec3f V2(m_HermiteVct2X, m_HermiteVct2Y, m_HermiteVct2Z);
+	ofPolyline line;
+	for (float t = 0.0f; t <= 1; t += 0.01)
+	{
+		line.addVertex((1 + 2 * t) * pow((1 - t), 2) * Pt1 + t * pow((1 - t), 2) * V1
+						+ pow(t, 2) * (t - 1) * V2 + pow(t, 2) * (3 - 2 * t) * Pt2);
+	}
+	ofSetHexColor(0xFF0000);
+	line.draw();
+	ofSetHexColor(0xFFFFFF);
 }
--- Project/CurvesBar.h
+++ Project/CurvesBar.h
@@ -17,12 +17,16 @@ public:
 	void Hide();
 
 	void AddBezierCubicCurve();
+	void AddHermiteCubicCurve();
 
 	void DrawBezierCurve();
+	void DrawHermiteCubicCurve();
+	void DrawHermite();
 
 private:
 	ofxPanel m_panel;
 	ofxButton m_BezierCubicButton;
+	ofxButton m_HermiteCubicButton;
 
 	bool m_visible;
 
@@ -30,16 +34,38 @@ private:
 	ofxPanel m_BezierCurvesPanel;
 	ofxFloatSlider m_BezierPt1X;
 	ofxFloatSlider m_BezierPt1Y;
+	ofxFloatSlider m_BezierPt1Z;
 	ofxFloatSlider m_BezierPt2X;
 	ofxFloatSlider m_BezierPt2Y;
+	ofxFloatSlider m_BezierPt2Z;
 	ofxFloatSlider m_BezierPt3X;
 	ofxFloatSlider m_BezierPt3Y;
+	ofxFloatSlider m_BezierPt3Z;
 	ofxFloatSlider m_BezierPt4X;
 	ofxFloatSlider m_BezierPt4Y;
+	ofxFloatSlider m_BezierPt4Z;
 	ofxButton m_CreateBezier;
 
+	// Hermite Cubic curves
+	ofxPanel m_HermiteCurvesPanel;
+	ofxFloatSlider m_HermitePt1X;
+	ofxFloatSlider m_HermitePt1Y;
+	ofxFloatSlider m_HermitePt1Z;
+	ofxFloatSlider m_HermitePt2X;
+	ofxFloatSlider m_HermitePt2Y;
+	ofxFloatSlider m_HermitePt2Z;
+	ofxFloatSlider m_HermiteVct1X;
+	ofxFloatSlider m_HermiteVct1Y;
+	ofxFloatSlider m_HermiteVct1Z;
+	ofxFloatSlider m_HermiteVct2X;
+	ofxFloatSlider m_HermiteVct2Y;
+	ofxFloatSlider m_HermiteVct2Z;
+	ofxButton m_CreateHermite;
+
 	bool m_BezierEnabled;
+	bool m_HermiteEnabled;
 	bool m_drawBezier;
+	bool m_drawHermite;
 	
 	Renderer *m_renderer;
 };
